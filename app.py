b983130f273d5c7aca0006921399e3945251770f
NightCat10759 <c107151159@nkust.edu.tw>
2021-06-17 16:33:32 +0800

    'ok'

--- app.py
+++ app.py
@@ -66,6 +66,9 @@ def handle_message(event):
     elif '功能列表' in msg:
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
+    elif '待辦' in msg:
+        message = ToDoList()
+        line_bot_api.reply_message(event.reply_token, '請問需要什麼服務呢')
     else:
         message = TextSendMessage(text=msg)
         line_bot_api.reply_message(event.reply_token, message)
