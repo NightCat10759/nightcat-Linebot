6a06e73fa1ddcc24ab8e1b3a3c027175e2603e4c
NightCat10759 <c107151159@nkust.edu.tw>
2021-06-29 03:27:56 +0800

    'ok'

--- app.py
+++ app.py
@@ -63,8 +63,10 @@ def handle_message(event):
     elif '刪除' in msg[0:2]:
         def DeleteTodo(Monthday,num,TodoDict) : #(月日,第幾個,待辦表)
             numLocal=num-1
-            del TodoDict[Monthday][numLocal]
-            message = TextSendMessage(text="刪除第"+str(num)+"項成功")
+  #          del TodoDict[Monthday][numLocal]
+ #           message = TextSendMessage(text="刪除第"+str(num)+"項成功")
+            message = TextSendMessage(text="num="+num+" numLocal=%d"%numLocal\
+                +"Monthday="+Monthday)
             return message
         message = DeleteTodo(msg[2:6],msg[7],Todo_dict) #(月日,第幾個,待辦表)
         line_bot_api.reply_message(event.reply_token, message)
