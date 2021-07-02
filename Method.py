9291162fca879988e95e50d20b076a7f80a249d8
NightCat10759 <c107151159@nkust.edu.tw>
2021-07-02 20:02:56 +0800

    'ok'

--- Method.py
+++ Method.py
@@ -28,40 +28,21 @@ def Help_template():
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
-                    title='這是第二塊模板',
-                    text='副標題可以自己改',
+                    thumbnail_image_url='https://imgs.gvm.com.tw/upload/gallery/20190911/68199010.jpg',
+                    title='高科大網址',
+                    text='按下以下標題可以進入該網址',
                     actions=[
-                        PostbackTemplateAction(
-                            label='回傳一個訊息',
-                            data='這是ID=2'
-                        ),
-                        MessageTemplateAction(
-                            label='用戶發送訊息',
-                            text='我知道這是2'
+                        URITemplateAction(
+                            label='高科大首頁',
+                            uri='https://www.nkust.edu.tw/'
                         ),
                         URITemplateAction(
-                            label='進入2的網頁',
-                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Number_2_in_light_blue_rounded_square.svg/200px-Number_2_in_light_blue_rounded_square.svg.png'
-                        )
-                    ]
-                ),
-                CarouselColumn(
-                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
-                    title='這是第三個模塊',
-                    text='最多可以放十個',
-                    actions=[
-                        PostbackTemplateAction(
-                            label='回傳一個訊息',
-                            data='這是ID=3'
-                        ),
-                        MessageTemplateAction(
-                            label='用戶發送訊息',
-                            text='我知道這是3'
+                            label='高科大教學平台',
+                            uri='https://elearning.nkust.edu.tw/mooc/index.php'
                         ),
                         URITemplateAction(
-                            label='uri2',
-                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png'
+                            label='高科大資工系',
+                            uri='http://www.csie.nkust.edu.tw/'
                         )
                     ]
                 )
