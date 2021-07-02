0cd917f962281046016b383f101a80b143cea3bb
NightCat10759 <c107151159@nkust.edu.tw>
2021-07-02 20:59:00 +0800

    'ok'

--- Method.py
+++ Method.py
@@ -87,7 +87,7 @@ def DeleteTodo(Monthday,num,TodoDict) : #(月日,第幾個,待辦表)
     else:
         message = TextSendMessage(text="日期必須為整數，詳細請打Help。")
         return message
-    
+    """
     # 建立待辦數區間
     num_0_max = (0,len(TodoDict[Monthday]))
     # 第幾個是否為整數
@@ -97,7 +97,7 @@ def DeleteTodo(Monthday,num,TodoDict) : #(月日,第幾個,待辦表)
     elif int(num) not in num_0_max:
         message = TextSendMessage(text="超出待辦範圍，詳細請打顯示(月日)。")
         return message
-    """
+    
     num=int(num) # 將第幾個轉換成數字
     numLocal=num-1
     del TodoDict[Monthday][numLocal]
