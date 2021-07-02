8108ef760904537d0a76614261bc5a0bfb152de1
NightCat10759 <c107151159@nkust.edu.tw>
2021-07-02 21:17:33 +0800

    'ok'

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def handle_message(event):
     elif '刪除' in msg[0:2]:
         if (msg[2:6].isdigit() == False):
             message = TextSendMessage(text="月份必須為整數，詳細請輸入Help。")
-            return message
+            line_bot_api.reply_message(event.reply_token, message)
         else:
             message = DeleteTodo(msg[2:6],msg[7],Todo_dict) #(月日,第幾個,待辦表)
             line_bot_api.reply_message(event.reply_token, message)
